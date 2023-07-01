4b0788ce326faca5e1ce30942d0922f4ba65ea7b
Liu He <lhe19@yahoo.co.uk>
2023-07-01 23:01:11 +0100

    added geocoder

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -3,6 +3,9 @@
 import React, { useState } from 'react';
 import Map from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import GeocoderControl from '@/components/geocoder';
+
+const mapBoxToken = process.env.NEXT_PUBLIC_MAPBOX!;
 
 export default function MapPage() {
   const [viewport, setViewport] = useState({
@@ -16,11 +19,19 @@ export default function MapPage() {
   return (
     <div className='h-screen'>
       <Map
-        {...viewport}
-        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX}
-        doubleClickZoom={false}
+        initialViewState={{
+          latitude: 46,
+          longitude: 17,
+          zoom: 4,
+        }}
+        mapboxAccessToken={mapBoxToken}
+        // doubleClickZoom={false}
         mapStyle='mapbox://styles/liuhe2020/cktu2h4q70wil17m6umh33a9i'
-      />
+        minZoom={1.585} // limit zoom out to single world map
+        maxZoom={19}
+      >
+        <GeocoderControl mapboxAccessToken={mapBoxToken} position='top-left' />
+      </Map>
     </div>
   );
 }
