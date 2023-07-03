9f07cd1ad8113de9b7040cbaee2f188d721cfde7
Liu He <lhe19@yahoo.co.uk>
2023-07-03 23:55:13 +0100

    added framer motion

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,19 +1,21 @@
 'use client';
 
-import React, { useState } from 'react';
-import Map, { type MapLayerMouseEvent, Marker } from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import React, { useState } from 'react';
+import Map, { type MapLayerMouseEvent, Marker, type ViewState, useMap } from 'react-map-gl';
 import GeocoderControl from '@/components/geocoder';
+import { AnimatePresence, motion } from 'framer-motion';
 
 const mapBoxToken = process.env.NEXT_PUBLIC_MAPBOX!;
+const ease = [[0.4, 0, 0.6, 1]];
 
 export default function MapPage() {
-  const [viewport, setViewport] = useState({
+  const [viewState, setViewState] = useState({
     latitude: 46,
     longitude: 17,
     zoom: 4,
-    minZoom: 1.585, // limit zoom out to single world map
-    maxZoom: 19,
+    // minZoom: 1.585, // limit zoom out to single world map
+    // maxZoom: 19,
   });
 
   const [newPin, setNewPin] = useState<null | { lat: number; long: number }>(null);
@@ -25,19 +27,17 @@ export default function MapPage() {
   };
 
   return (
-    <div className='h-screen'>
+    <div className='h-screen overflow-hidden relative'>
       <Map
-        initialViewState={{
-          latitude: 46,
-          longitude: 17,
-          zoom: 4,
-        }}
+        {...viewState}
+        onMove={(evt) => setViewState(evt.viewState)}
         mapboxAccessToken={mapBoxToken}
         // doubleClickZoom={false}
         mapStyle='mapbox://styles/liuhe2020/cktu2h4q70wil17m6umh33a9i'
         minZoom={1.585} // limit zoom out to single world map
         maxZoom={19}
         onClick={handleMapClick}
+        style={{ width: `${newPin ? '70%' : '100%'}` }}
       >
         <GeocoderControl mapboxAccessToken={mapBoxToken} position='top-left' />
         {newPin && (
@@ -50,6 +50,15 @@ export default function MapPage() {
           </Marker>
         )}
       </Map>
+      {/* {newPin && (
+        <motion.div
+          className='absolute right-0 top-0 w-96 h-full z-10 bg-white'
+          initial={{ x: '100%' }}
+          animate={{ x: 0 }}
+          exit={{ x: '100%' }}
+          transition={{ duration: 0.5, ease }}
+        ></motion.div>
+      )} */}
     </div>
   );
 }
