9535c1ab04b7d7a6854d8f9d816a4c2dc4f59f6a
Liu He <lhe19@yahoo.co.uk>
2023-07-04 23:58:34 +0100

    working out flyTo position

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,10 +1,11 @@
 'use client';
 
 import 'mapbox-gl/dist/mapbox-gl.css';
-import React, { useState } from 'react';
-import Map, { type MapLayerMouseEvent, Marker, type ViewState, useMap } from 'react-map-gl';
+import React, { useCallback, useRef, useState } from 'react';
+import Map, { Marker, type MapLayerMouseEvent, type MapRef } from 'react-map-gl';
 import GeocoderControl from '@/components/geocoder';
 import { AnimatePresence, motion } from 'framer-motion';
+import { cn } from '@/lib/utils';
 
 const mapBoxToken = process.env.NEXT_PUBLIC_MAPBOX!;
 const ease = [[0.4, 0, 0.6, 1]];
@@ -17,18 +18,29 @@ export default function MapPage() {
     // minZoom: 1.585, // limit zoom out to single world map
     // maxZoom: 19,
   });
+  const [width, setWidth] = useState('w-full');
+
+  const mapRef = useRef<MapRef>(null);
 
   const [newPin, setNewPin] = useState<null | { lat: number; long: number }>(null);
 
   // create a new marker at clicked location
   const handleMapClick = (e: MapLayerMouseEvent) => {
-    if (newPin) return setNewPin(null);
+    if (newPin) {
+      // setWidth('w-full');
+      setNewPin(null);
+      return;
+    }
+    // setWidth('w-3/4');
     setNewPin({ lat: e.lngLat.lat, long: e.lngLat.lng });
+    mapRef.current?.resize();
+    mapRef.current?.flyTo({ center: [e.lngLat.lng + 8, e.lngLat.lat] });
   };
 
   return (
-    <div className='h-screen overflow-hidden relative'>
+    <div className={`h-screen overflow-hidden relative flex`}>
       <Map
+        ref={mapRef}
         {...viewState}
         onMove={(evt) => setViewState(evt.viewState)}
         mapboxAccessToken={mapBoxToken}
@@ -37,7 +49,6 @@ export default function MapPage() {
         minZoom={1.585} // limit zoom out to single world map
         maxZoom={19}
         onClick={handleMapClick}
-        style={{ width: `${newPin ? '70%' : '100%'}` }}
       >
         <GeocoderControl mapboxAccessToken={mapBoxToken} position='top-left' />
         {newPin && (
@@ -50,15 +61,15 @@ export default function MapPage() {
           </Marker>
         )}
       </Map>
-      {/* {newPin && (
+      {newPin && (
         <motion.div
-          className='absolute right-0 top-0 w-96 h-full z-10 bg-white'
-          initial={{ x: '100%' }}
-          animate={{ x: 0 }}
-          exit={{ x: '100%' }}
+          className='h-full z-10 bg-white'
+          initial={{ width: 0 }}
+          animate={{ width: 384 }}
+          exit={{ width: 0 }}
           transition={{ duration: 0.5, ease }}
         ></motion.div>
-      )} */}
+      )}
     </div>
   );
 }
