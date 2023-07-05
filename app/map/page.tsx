650269203880225717f512f398c89e6049184bf7
Liu He <lhe19@yahoo.co.uk>
2023-07-05 23:17:13 +0100

    changed to easeTo on drawer open/close

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import 'mapbox-gl/dist/mapbox-gl.css';
-import React, { useCallback, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import Map, { Marker, type MapLayerMouseEvent, type MapRef } from 'react-map-gl';
 import GeocoderControl from '@/components/geocoder';
 import { AnimatePresence, motion } from 'framer-motion';
@@ -18,7 +18,6 @@ export default function MapPage() {
     // minZoom: 1.585, // limit zoom out to single world map
     // maxZoom: 19,
   });
-  const [width, setWidth] = useState('w-full');
 
   const mapRef = useRef<MapRef>(null);
 
@@ -33,37 +32,38 @@ export default function MapPage() {
     }
     // setWidth('w-3/4');
     setNewPin({ lat: e.lngLat.lat, long: e.lngLat.lng });
-    mapRef.current?.panBy([50, 0]);
-    // mapRef.current?.flyTo({ center: [e.lngLat.lng + 8, e.lngLat.lat], speed: 0.3 });
+    mapRef.current?.easeTo({ center: [e.lngLat.lng + 8, e.lngLat.lat] });
   };
 
   return (
-    <div className={`h-screen overflow-hidden relative flex`}>
-      <Map
-        ref={mapRef}
-        {...viewState}
-        onMove={(evt) => setViewState(evt.viewState)}
-        mapboxAccessToken={mapBoxToken}
-        // doubleClickZoom={false}
-        mapStyle='mapbox://styles/liuhe2020/cktu2h4q70wil17m6umh33a9i'
-        minZoom={1.585} // limit zoom out to single world map
-        maxZoom={19}
-        onClick={handleMapClick}
-      >
-        <GeocoderControl mapboxAccessToken={mapBoxToken} position='top-left' />
-        {newPin && (
-          <Marker
-            latitude={newPin.lat}
-            longitude={newPin.long}
-            offset={[0, -14]} //centering marker
-          >
-            <img src='/images/marker_blue.svg' alt='marker_pin' style={{ cursor: 'pointer' }} />
-          </Marker>
-        )}
-      </Map>
+    <div className='overflow-hidden relative'>
+      <div className='w-full h-screen'>
+        <Map
+          ref={mapRef}
+          {...viewState}
+          onMove={(evt) => setViewState(evt.viewState)}
+          mapboxAccessToken={mapBoxToken}
+          // doubleClickZoom={false}
+          mapStyle='mapbox://styles/liuhe2020/cktu2h4q70wil17m6umh33a9i'
+          minZoom={1.585} // limit zoom out to single world map
+          maxZoom={19}
+          onClick={handleMapClick}
+        >
+          <GeocoderControl mapboxAccessToken={mapBoxToken} position='top-left' />
+          {newPin && (
+            <Marker
+              latitude={newPin.lat}
+              longitude={newPin.long}
+              offset={[0, -14]} //centering marker
+            >
+              <img src='/images/marker_blue.svg' alt='marker_pin' style={{ cursor: 'pointer' }} />
+            </Marker>
+          )}
+        </Map>
+      </div>
       {newPin && (
         <motion.div
-          className='h-full z-10 bg-white'
+          className='absolute right-0 top-0 h-full z-10 bg-white'
           initial={{ width: 0 }}
           animate={{ width: 384 }}
           exit={{ width: 0 }}
