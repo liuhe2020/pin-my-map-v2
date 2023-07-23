67b6c03194475c1c80b980b2976f609ef12fc33a
Liu He <lhe19@yahoo.co.uk>
2023-07-23 17:54:10 +0100

    added lightbox package

--- app/map/[userId]/pin.tsx
+++ app/map/[userId]/pin.tsx
@@ -1,5 +1,8 @@
 import { pinAtom } from '@/lib/atoms';
 import { useAtom } from 'jotai';
+import Image from 'next/image';
+import { Lightbox } from 'yet-another-react-lightbox';
+import 'yet-another-react-lightbox/styles.css';
 
 export default function Pin() {
   const [pin] = useAtom(pinAtom);
@@ -30,7 +33,15 @@ export default function Pin() {
         <h2>Description</h2>
         <p className=''>{pin?.description}</p>
       </div>
-      <div className='space-y-2'></div>
+      <div className='space-y-2'>
+        <h2>Photos</h2>
+        <div className='grid grid-cols-3 gap-2'>
+          {pin?.photos.map((photo) => (
+            <Image key={photo.id} src={photo.url} alt={pin.location} width={140} height={140} className='aspect-square object-cover cursor-pointer' />
+          ))}
+        </div>
+      </div>
+      <Lightbox open={true} slides={pin?.photos.map((photo) => ({ src: photo.url }))} />
     </div>
   );
 }
--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: { serverActions: true },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/pin-my-map/image/upload/**',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
